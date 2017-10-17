0827e7aaae69b2dbd77b68aad8fb45cd95a41d53
Varun Gandhi <vg266@cornell.edu>
2017-10-17 16:38:56 -0400

    Replaces xrange with range (Python 3), small refactoring, removes MC code.
    Also replaces `==None` with `is None` (Numpy fails in Python3 for direct
    comparison with None, this was just a future warning in Python2).

--- src/chromatinMD.py
+++ src/chromatinMD.py
@@ -56,7 +56,7 @@ class strand( object ):
            r (columns) is ordered as {x,y,z,psi}.
            * We don't have psi depend on r.
            ** We should check with * numerically.""" 
-        if tangent==None: tangent=self.tangent()
+        if tangent is None: tangent=self.tangent()
         t = tangent
         D = np.sqrt( t[...,0]**2 + t[...,1]**2 + t[...,2]**2 )
         p = np.sqrt( t[...,0]**2 + t[...,1]**2 ) #+ 1.E-16
@@ -75,7 +75,7 @@ class strand( object ):
 
     def removeLocalStretch( self, tangent=None ):
         """ Updates r vector to remove local stretch """
-        if tangent==None: tangent=self.tangent()
+        if tangent is None: tangent=self.tangent()
         t = normalize( tangent, self.d )
         self.r[:, :3] = np.cumsum( t, axis=0 )
 
@@ -93,29 +93,29 @@ def rDot( r, time, strandClass, tangent=None, jacobian=None, torques=None,
         Evolves [r_1, ..., r_L-1] components of r. r_0 is immobile.
         Enter r in one-dimensional shape: (4 (L-1),)
         [x1, y1, z1, psi1, ..., x_{L-1}, y_{L-1}, z_{L-1}, psi_{L-1}]
-        Inexitensible is true if the strand does not stretch locally."""
+        inextensible is true if the strand does not stretch locally."""
     strandClass.r = r.reshape(( strandClass.L, 4 ))
 
-    if params==None: params = parameters( strandClass )
+    if params is None: params = parameters( strandClass )
     cR, cPsi = params
 
-    if tangent==None: tangent = strandClass.tangent()
+    if tangent is None: tangent = strandClass.tangent()
 
-    if jacobian==None: jacobian = strandClass.jacobian( tangent=tangent )
+    if jacobian is None: jacobian = strandClass.jacobian( tangent=tangent )
     J = jacobian
 
-    if torques==None:
+    if torques is None:
         dnaAngle = angular( strandClass, tangent=tangent )
         torques = dnaAngle.effectiveTorques()
     tau = torques
 
     x = 0.0 * tau
-    for i in xrange(3):
-        for j in xrange(3):
+    for i in range(3):
+        for j in range(3):
             x[:,i] += J[:, j, i] * tau[:, j]
 
     drdt = np.zeros(( strandClass.L, 4 ))
-    for i in xrange(3):
+    for i in range(3):
         drdt[1:,i] -= cR * ( x[1:,i] - x[:-1,i] )
 #    drdt[-1, 2] += cR * force
     drdt[-1,:3] += cR * force * tangent[-1] / strandClass.d
@@ -141,31 +141,27 @@ def makeFilename( directory, elementList, extension, dated=True ):
         Examples of extension: 'png' (without '.')."""
     filename = directory
     if dated:
-        x = str( datetime.datetime.today() ).split()
-        y = x[0].split('-') + x[1].split('.')[0].split(':')[:2]
-        filename += ''.join( x[0].split('-') + x[1].split('.')[0].split(':')[:2] )
+        day, time = str( datetime.datetime.today() ).split()
+        y = day.split('-') + time.split('.')[0].split(':')[:2]
+        filename += ''.join(y)
         filename += '-'
     filename += '-'.join( elementList )
     filename += '.' + extension
     return filename
 
-def projectPerp( vecA, vecB ):
-    """ Returns vecA - ( vecA . vecB ) vecB.
-        Vectors A and B must have shape (N,3)."""
-    AdotB = vecA[:,0]*vecB[:,0] + vecA[:,1]*vecB[:,1] + vecA[:,2]*vecB[:,2]
-    vec = 1.0*vecA
-    for i in xrange(3):
-        vec[:,i] -= AdotB * vecB[:,i]
-    return vec
-
 def project( vecA, vecB ):
     """ Returns ( vecA . vecB ) vecB.
         Vectors A and B must have shape (N,3)."""
     AdotB = vecA[:,0]*vecB[:,0] + vecA[:,1]*vecB[:,1] + vecA[:,2]*vecB[:,2]
-    vec = 1.0*vecB
-    for i in xrange(3):
+    vec = vecB.copy()
+    for i in range(3):
         vec[:,i] *= AdotB
     return vec
+
+def projectPerp( vecA, vecB ):
+    """ Returns vecA - ( vecA . vecB ) vecB.
+        Vectors A and B must have shape (N,3)."""
+    return vecA - project( vecA, vecB )
 
 def normalize( vector, N=1.0 ):
     """ Returns vector field with norm N.
@@ -173,7 +169,7 @@ def normalize( vector, N=1.0 ):
     norm = np.sqrt( vector[:,0]**2 + vector[:,1]**2 + vector[:,2]**2 )
 
     x = N * vector
-    for i in xrange(3):
+    for i in range(3):
         x[:,i] /= norm
     return x
 
@@ -191,7 +187,7 @@ class angular( object ):
 
     def alpha( self, strandClass, tangent=None ):
         """ alpha = {Delta, phi, theta, psi}."""
-        if tangent==None:
+        if tangent is None:
             t = strandClass.tangent()
         else:
             t = tangent
@@ -284,18 +280,18 @@ class angular( object ):
     def effectiveTorques( self, Rs=None, DRs=None ):
         """ Returns the effective torques per temperature."""
         RLp1 = np.zeros(( self.L+1, 3, 3))
-        if Rs==None:
+        if Rs is None:
             RLp1[:-1,...] = self.rotationMatrices()
         else:
             RLp1[:-1,...] = Rs
         RLp1[-1,...] = self.RL
 
-        if DRs==None: DRs = self.derivativeRotationMatrices()
+        if DRs is None: DRs = self.derivativeRotationMatrices()
 
         tau = np.zeros(( self.L, 4))
-        for i in xrange(3):
-            for j in xrange(3):
-                for k in xrange(3):
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
                     if k==2:
                         c = -( self.C + 2.0*self.B ) / ( 2.0*self.d ) 
                     else:
@@ -307,7 +303,7 @@ class angular( object ):
     def deltaMatrices( self, Rs=None ):
         """ Returns delta matrices. """
         RLp1 = np.zeros(( self.L+1, 3, 3))
-        if Rs==None:
+        if Rs is None:
             RLp1[:-1,...] = self.rotationMatrices()
         else:
             RLp1[:-1,...] = Rs
@@ -317,197 +313,4 @@ class angular( object ):
         b = RLp1[1:]
         return np.array([ np.dot(a[i], b[i]) for i in xrange(len(a)) ])
 
-#
-# MC stuff
-#
-class nakedDNA( object ):
-    """ Creates the naked DNA class.
-        Euler angles are ordered as phi, theta and psi.
-        Euler angle parametrization... """
-    def __init__(self, L=740, B=43.0, C=89.0, d=1.0):
-        self.L = L
-        self.B = B
-        self.C = C
-        self.d = d
-        self.euler = np.zeros(( self.L, 3))
-        self.oddMask = np.array([ i & 0x1 for i in xrange( self.L -2 ) ], dtype=bool)
-        self.evenMask = np.roll( self.oddMask, 1 )
-
-    def rotationMatrices( self ):
-        """ Returns rotation matrices along the DNA string"""
-        phi = self.euler[...,0]
-        theta = self.euler[...,1]
-        psi = self.euler[...,2]
-        R = np.zeros(( self.L, 3, 3 ))
-        R[..., 0, 0] = np.cos(phi) * np.cos(psi) - np.cos(theta) * np.sin(phi) * np.sin(psi)
-        R[..., 1, 0] = -np.cos(psi) * np.sin(phi) - np.cos(theta) * np.cos(phi) * np.sin(psi)
-        R[..., 2, 0] = np.sin(theta) * np.sin(psi)
-        R[..., 0, 1] = np.cos(phi) * np.sin(psi) + np.cos(theta) * np.cos(psi) * np.sin(phi)
-        R[..., 1, 1] = -np.sin(phi) * np.sin(psi) + np.cos(theta) * np.cos(phi) * np.cos(psi)
-        R[..., 2, 1] = -np.cos(psi) * np.sin(theta)
-        R[..., 0, 2] = np.sin(theta) * np.sin(phi)
-        R[..., 1, 2] = np.cos(phi) * np.sin(theta)
-        R[..., 2, 2] = np.cos(theta)
-        return R
-
-    def deltaMatrices( self, Rs=None ):
-        """ Returns delta matrices. """
-        if Rs==None:
-            Rs = self.rotationMatrices()
-        a = np.swapaxes( Rs[:-1], 1, 2 )
-        b = Rs[1:]
-        return np.array([ np.dot(a[i], b[i]) for i in xrange(len(a)) ])
-
-    def twistBendAngles( self, Ds=None, squared=True ):
-        """ Returns the twist and bending angles."""
-        if Ds==None:
-            Ds = self.deltaMatrices()
-        if squared:
-            betaSq = 2.0 * ( 1 - Ds[...,2,2] )
-            GammaSq = 1.0 - Ds[...,0,0] - Ds[...,1,1] + Ds[...,2,2]
-            return betaSq, GammaSq
-        else:
-            beta1 = ( Ds[...,1,2] - Ds[...,2,1] ) / 2.0
-            beta2 = ( Ds[...,2,0] - Ds[...,0,2] ) / 2.0
-            Gamma = ( Ds[...,0,1] - Ds[...,1,0] ) / 2.0
-            return beta1, beta2, Gamma
-
-    def bendingEnergyDensity( self, angles=None, squared=True ):
-        """ Returns the bending energy density.
-            Enter angles in a tuple( arrays ) format."""
-        if angles==None:
-            angles = self.twistBendAngles( squared=squared )
-
-        if squared:
-            return self.B * angles[0] / (2.*self.d)
-        else:
-            return self.B * ( angles[0]**2 + angles[1]**2 ) / (2.*self.d)
-
-    def bendingEnergy( self, squared=True, bendEnergyDensity=None ):
-        """ Returns the total bending energy."""
-        if bendEnergyDensity==None:
-            bendEnergyDensity = self.bendingEnergyDensity( squared=squared )
-
-        return np.sum( bendEnergyDensity )
-
-    def twistEnergyDensity( self, angles=None, squared=True ):
-        """ Returns the twist energy density."""
-        if angles==None:
-            angles = self.twistBendAngles( squared=squared )
-        if squared:
-            return self.C * angles[-1] / (2.*self.d)
-        else:
-            return self.C * angles[-1]**2 / (2.*self.d)
-
-    def twistEnergy( self, squared=True, twistEnergyDensity=None ):
-        """ Returns the total twist energy. """
-        if twistEnergyDensity==None:
-            twistEnergyDensity = self.twistEnergyDensity( squared=squared )
-
-        return np.sum( twistEnergyDensity )
-
-    def stretchEnergyDensity( self, tangent=None, force=1.96 ):
-        """ Returns the stretching energy density.
-            Enter the force in pN
-            Our energy is in unit of kT.
-            Es = force * tangent * prefactor
-            prefactor = 10E-12 10-9/ (1.38E-23 296.65).
-            Change prefactor to change the temperature."""
-        prefactor = 0.24
-        if tangent==None:
-            tangent = self.tVector()
-        return -force * prefactor * tangent[:-1,2]
-
-    def stretchEnergy( self, force=1.96, stretchEnergyDensity=None ):
-        """ Returns the total stretching energy. """
-        if stretchEnergyDensity==None:
-            stretchEnergyDensity = self.stretchEnergyDensity( force=force )
-        return np.sum( stretchEnergyDensity )
-
-    def totalEnergyDensity( self, squared=True, force=1.96 ):
-        """ Returns the total energy density."""
-        E = self.bendingEnergyDensity( squared=squared )
-        E += self.twistEnergyDensity( squared=squared ) 
-        E += self.stretchEnergyDensity( force=force )
-        return E 
-
-    def totalEnergy( self, squared=True, force=1.96, totalEnergyDensity=None ):
-        """ Returns the total energy. """
-        if totalEnergyDensity==None:
-            totalEnergyDensity=self.totalEnergyDensity( squared=squared, force=force )
-        return np.sum( totalEnergyDensity )
-
-    def tVector( self ):
-        """ Returns the tangent vectors. """
-        ( phi, theta ) = ( self.euler[...,0], self.euler[...,1] )
-        t = np.zeros(( self.L, 3 ))
-        t[..., 0] = np.sin(theta) * np.sin(phi)
-        t[..., 1] = np.cos(phi) * np.sin(theta)
-        t[..., 2] = np.cos(theta)        
-        return t
-
-    def rVector( self, t=None ):
-        """ Returns the end points of the t-vectors."""
-        if t==None:
-            t = self.tVector()
-        return np.cumsum( t, axis=0 ) 
-
-def metropolisUpdate( dnaClass, sigma=0.1, squared=True, force=1.96, E0=None ):
-    """ Update dnaClass Euler angles using Metropolis algorithm.
-        Return the total energy density. """
-    if E0==None:
-        E0 = dnaClass.totalEnergyDensity( squared=squared, force=force )
-
-    moves = sigma * np.random.randn( dnaClass.L-2, 3 )
-    moves *= ( np.abs(moves) < 5.0*sigma )
-
-    for i in xrange(3):
-        dnaClass.euler[1:-1, i] += moves[..., i] * dnaClass.oddMask
-        Ef = dnaClass.totalEnergyDensity( squared=squared, force=force )
-        deltaE = ( Ef - E0 )[:-1] + ( Ef - E0 )[1:]
-
-        reject = 1.0 * dnaClass.oddMask
-        reject *= deltaE > 0
-        #reject *= np.random.rand( dnaClass.L - 2 ) > np.exp( - deltaE )
-
-        dnaClass.euler[1:-1,i] -= moves[..., i] * reject
-
-        E0 = dnaClass.totalEnergyDensity( squared=squared, force=force )
-        
-        dnaClass.euler[1:-1, i] += moves[..., i] * dnaClass.evenMask
-
-        Ef = dnaClass.totalEnergyDensity( squared=squared, force=force )
-        deltaE = ( Ef - E0 )[:-1] + ( Ef - E0 )[1:]
-
-        reject = 1.0 * dnaClass.evenMask
-        reject *= deltaE > 0
-        #reject *= np.random.rand( dnaClass.L - 2 ) > np.exp( - deltaE )
-
-        dnaClass.euler[1:-1,i] -= moves[..., i] * reject
-        E0 = dnaClass.totalEnergyDensity( squared=squared, force=force )   
-    return E0
-
-def mcRelaxation( dnaClass, sigma=0.1, squared=True, force=1.96, E0=None, mcSteps=100 ):
-    """ Monte Carlo relaxation using Metropolis algorithm. """
-    energyList = []
-    xList = []
-    if E0==None:
-        E0 = dnaClass.totalEnergyDensity( squared=squared, force=force )
-    energyList.append( np.sum( E0 ) )
-    for i in xrange(mcSteps):
-        E0 = metropolisUpdate( dnaClass, sigma, squared, force, E0 )
-        energyList.append( np.sum(E0) )
-        xList.append( np.sum( dnaClass.tVector()[:,2] ) )
-    return np.array( energyList ), np.array( xList )
-
-def torsionProtocol( dnaClass, sigma=0.1, squared=True, force=1.96, E0=None, mcSteps=100,
-            twists=np.arange( np.pi/2.0 , 30.0 * np.pi, np.pi/2.0 ) ):
-    """ Simulate a torsion protocol defined by twists. """
-    energyList, extensionList = [], [] 
-    for x in twists:
-        dnaClass.euler[-1, 2] = x
-        energy, extension= mcRelaxation( dnaClass, sigma, squared, force, E0, mcSteps )
-        energyList.append( energy[-1] )
-        extensionList.append( extension[-1] )
-    return energyList, extensionList
-
+        return a @ b
